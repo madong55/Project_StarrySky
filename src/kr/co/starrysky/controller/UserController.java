d07efee579dd1b9a0918e8822ffa89e9fa938c54
madong55 <sww4507@gmail.com>
2021-03-23 14:15:34 +0900

    hahiha삭제

--- src/kr/co/starrysky/controller/UserController.java
+++ src/kr/co/starrysky/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.co.starrysky.beans.UserBean;
 import kr.co.starrysky.service.UserService;
@@ -28,9 +30,14 @@ public class UserController {
 	private UserBean loginUserBean;
 	
 	@GetMapping("/login")
-	public String login(@ModelAttribute("tempLoginUserBean") UserBean tempLoginUserBean) {
+	public String login(@ModelAttribute("tempLoginUserBean") UserBean tempLoginUserBean,
+						@RequestParam(value = "fail", defaultValue="false") boolean fail, Model model) {
 		
-		return "user/login";
+		//로그인fail이 true인 경우 : 로그인실패
+		//로그인fail이 false인 경우 : 로그인성공
+		model.addAttribute("fail", fail);
+		
+		return "user/login"; 
 	}
 	
 	@PostMapping("/login_pro")
