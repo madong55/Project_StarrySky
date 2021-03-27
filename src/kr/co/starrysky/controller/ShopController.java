dfb85cce9c6bd4ebdfb48e0a935f21f48d184678
Allan Jang <hwonttl@gmail.com>
2021-03-27 15:19:46 +0900

    shopcontroller update

--- src/kr/co/starrysky/controller/ShopController.java
+++ src/kr/co/starrysky/controller/ShopController.java
@@ -1,7 +1,9 @@
 package kr.co.starrysky.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class ShopController {
 
 	@GetMapping("/shop")
