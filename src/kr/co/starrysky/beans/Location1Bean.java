bec526595ada571c64d99cf3f8cb3cdfdc65c64c
Allan Jang <hwonttl@gmail.com>
2021-04-02 11:19:42 +0900

    beans are added.

---
+++ src/kr/co/starrysky/beans/Location1Bean.java
@@ -0,0 +1,23 @@
+package kr.co.starrysky.beans;
+
+public class Location1Bean {
+	private String location1_name;
+	private int location1_id;
+	
+	
+	public String getLocation1_name() {
+		return location1_name;
+	}
+	public void setLocation1_name(String location1_name) {
+		this.location1_name = location1_name;
+	}
+	public int getLocation1_id() {
+		return location1_id;
+	}
+	public void setLocation1_id(int location1_id) {
+		this.location1_id = location1_id;
+	}
+	
+	
+	
+}
